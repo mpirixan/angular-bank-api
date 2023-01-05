095981d91733e0012489a7857e8b1d2a4efd6106
mpirixan <matheus.pirixan@techlead.com.br>
2023-01-04 23:52:13 -0300

    init commit

---
+++ src/main/java/com/learn/angularbankapi/AngularBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.angularbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularBankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/angularbankapi/AngularBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.angularbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
